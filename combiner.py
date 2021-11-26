e2f3d21120b475397c52632d61f390aa0aaf6fba
Ganer <34655998+GanerCodes@users.noreply.github.com>
2021-11-26 02:55:21 -0600

    asdf

--- combiner.py
+++ combiner.py
@@ -1,18 +1,14 @@
-from ffprobe import FFProbe as ffprobe
-import ffmpeg
-import subprocess as sp
+import ffmpeg  # Nice and simple
+from ffprobe import FFProbe as ffprobe # eew ugle ):<
 
-def exec(cmd):
-    return sp.Popen(cmd, stdout = sp.PIPE, stderr = sp.PIPE).communicate()
-
-def combiner(videos, output = "video.mp4"):
+def combiner(videos, output = "video.mp4", SILENCE = "./SILENCE.mp3"):
     maxRes = (0, 0)
     maxFPS = 0
     filteredVids = []
     for vid in videos:
         r = ffprobe(vid)
         if r.video:
-            newVid = (vid, vm := r.video[0], bool(r.audio))
+            newVid = (vid, vm := r.video[0], r.audio)
             maxRes = (max(int(vm.width), maxRes[0]), max(int(vm.height), maxRes[1]))
             maxFPS = max(vm.framerate, maxFPS)
             filteredVids.append(newVid)
@@ -20,21 +16,23 @@ def combiner(videos, output = "video.mp4"):
             print(f"""File {vid} does not contain any video streams, skipping.""")
     assert len(filteredVids) > 0, "Error: Found no suitable videos."
     
-    for filename, properties, hasAudio in filteredVids:
-        print(filename)
-        # if properties.framerate != maxFPS:
-            #
+    preparedVids = []
+    for filename, vidProps, audProps in filteredVids:
+        f = ffmpeg.input(filename).video
+        if (size := (vidProps.width, vidProps.height)) != maxRes:
+            f = f.filter('scale', size = f"{maxRes[0]}x{maxRes[1]}", force_original_aspect_ratio = "decrease")
+            f = f.filter('pad', maxRes[0], maxRes[1], "(ow-iw)/2", "(oh-ih)/2")
+        preparedVids.append(f)
+        if audProps:
+            preparedVids.append(ffmpeg.input(filename).audio)
+        else:
+            preparedVids.append(ffmpeg.input(SILENCE).audio)
     
-    print(filteredVids)
-    print(maxRes, maxFPS)
+    final = ffmpeg.concat(*preparedVids, n = len(filteredVids), v = 1, a = 1).output(output).global_args("-y").global_args("-vsync", "2").global_args("-hide_banner").global_args("-loglevel", "error")
+    final.run()
+    print(f"Finished! Exported video ({maxRes[0]}x{maxRes[1]}p{maxFPS})")
 
 if __name__ == "__main__":
+    from os import listdir
     from sys import argv
-    #do args stuff
-    
-    videos = [
-        r"C:\Users\Administrator\Documents\Projects\Video_Combiner\bruh\1631887446622.webm",
-        r"C:\Users\Administrator\Documents\Projects\Video_Combiner\bruh\fasdf.mp4"
-    ]
-    output = r"C:\Users\Administrator\Documents\Projects\Video_Combiner"
-    combiner(videos, output)
+    combiner(argv[2:] if len(argv) > 2 else sorted(["videos/"+i for i in listdir("videos")]), argv[1] if len(argv) > 1 else "video.mp4")
